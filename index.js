c82c651ddf83b000e4a70a009076c2fe9861a644
lupenq <skinilya@gmail.com>
2020-04-10 22:15:48 +0300

    Create simple http-server

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
+  if (req.method === 'GET') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    res.end(`
+      <h1>Form</h1>
+      <form method="POST" action="/">
+        <input type="text" name="title" />
+        <button type="submit">Send</button>
+      </form>
+    `)
+  } else if (req.method === 'POST') {
+    const body = []
 
-  res.write('Hello from NodeJs')
-  res.end()
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8',
+    })
+
+    req.on('data', data => {
+      body.push(Buffer.from(data))
+    })
+
+    req.on('end', () => {
+      const message = body.toString().split('=')[1]
+
+      res.end(`
+        <h1>Ваше сообщение: ${message}</h1>
+    `)
+    })
+  }
 })
 
 server.listen(3001, () => {
