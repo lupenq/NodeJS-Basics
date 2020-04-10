46cff17b3e031da556def8865f35a17420d2c1a6
lupenq <skinilya@gmail.com>
2020-04-10 22:39:57 +0300

    test simple REST-server

--- index.js
+++ index.js
@@ -30,6 +30,17 @@ const server = http.createServer((req, res) => {
           res.end(content)
         }
       )
+    } else if (req.url === '/api/users') {
+      res.writeHead(200, {
+        'Content-Type': 'text/json'
+      })
+
+      const users = [
+        {name: 'Ilya', age: 22},
+        {name: 'Eva', age: 19}
+      ]
+
+      res.end(JSON.stringify(users))
     }
   } else if (req.method === 'POST') {
     const body = []
